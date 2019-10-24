4051a333d166e10cec47eb07c6bfd635c76b372e
Xu <lei.xu02@sap.com>
2019-10-24 09:38:10 +0800

    :art: add fizzbuzz

--- src/main/java/com/lei/learn/tdd/FizzBuzz/FizzBuzz.java
+++ src/main/java/com/lei/learn/tdd/FizzBuzz/FizzBuzz.java
@@ -1,4 +1,16 @@
 package com.lei.learn.tdd.FizzBuzz;
 
 public class FizzBuzz {
+  public String print(int input) {
+    if (input % 15 == 0) {
+      return "FizzBuzz";
+    }
+    if (input % 3 == 0) {
+      return "Fizz";
+    }
+    if (input % 5 == 0) {
+      return "Buzz";
+    }
+    return String.valueOf(input);
+  }
 }
--- src/test/java/com/lei/learn/tdd/FizzBuzz/FizzBuzzTest.java
+++ src/test/java/com/lei/learn/tdd/FizzBuzz/FizzBuzzTest.java
@@ -1,4 +1,37 @@
 package com.lei.learn.tdd.FizzBuzz;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class FizzBuzzTest {
+
+  @Test
+  public void should_see_one_print() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    String result = fizzBuzz.print(1);
+    assertThat(result, is("1"));
+  }
+
+  @Test
+  public void should_see_fizz_print() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    String result = fizzBuzz.print(3);
+    assertThat(result, is("Fizz"));
+  }
+
+  @Test
+  public void should_see_buzz_print() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    String result = fizzBuzz.print(5);
+    assertThat(result, is("Buzz"));
+  }
+
+  @Test
+  public void should_see_fizzbuzz_print() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    String result = fizzBuzz.print(15);
+    assertThat(result, is("FizzBuzz"));
+  }
 }
